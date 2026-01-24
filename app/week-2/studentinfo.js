12adb56a2b6335823a7973ebc59c4b2359f72f21
Aaron Reid <aaron.reid@edu.sait.ca>
2026-01-23 20:52:41 -0700

    created folder named week2, along with a studentinfo component

---
+++ app/week-2/studentinfo.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+export default function StudentInfo(){
+    <main>
+        <h1>Aaron</h1>
+        <Link href="https://github.com/aaron-reid19/cprg306-assignments">Github repository</Link>
+    </main>
+}
