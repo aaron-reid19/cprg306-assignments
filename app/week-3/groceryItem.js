4ff63905ffaa3a8e3064273a953ea1a33231dc32
Aaron Reid <aaron.reid@edu.sait.ca>
2026-01-30 22:30:17 -0700

    changed the styling of the grocery items

--- app/week-3/groceryItem.js
+++ app/week-3/groceryItem.js
@@ -1,7 +1,7 @@
 export default function Item({name, quantity, category}){
   return (
     
-    <ul className="border-2 rounded-md px-3 py-2 m-4 ">
+    <ul className="border-2 rounded-md px-3 py-2 m-4 w-64 mx-auto">
         <li>{ name }</li>
         <li>Quantity: { quantity }</li>
         <li>Category: { category }</li>
