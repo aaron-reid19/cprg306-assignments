54abd0bef655c8a231c4c14bcbad00aae7bb6f9e
Aaron Reid <aaron.reid@edu.sait.ca>
2026-02-06 23:32:50 -0700

    implemented week 4 folder, with item.js, item-list.js and page.js

---
+++ app/week-4/item.js
@@ -0,0 +1,10 @@
+export default function Item({name, quantity, category}){
+    return(
+        <ul className="border-2 rounded-md px-3 py-2 m-4 w-64 mx-auto">
+            <li>{name}</li>
+            <li>Quantity: { quantity }</li>
+            <li>Category: { category }</li>
+        </ul>
+    )
+    
+}
---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+
+export default function Page(){
+    return(
+        <main>
+            <ItemList />
+        </main>
+        
+    )
+}
